e6e31dd944091eb4d146a3f25e07c65c0e37ad58
Sistemas Free <ana.info1999@gmail.com>
2023-10-17 14:37:59 -0500

    Update config

--- Config/Config.php
+++ Config/Config.php
@@ -7,9 +7,5 @@ const DB = "shop";
 const CHARSET = "charset=utf8";
 const TITLE = "SHOP";
 const MONEDA = "USD";
-const CLIENT_ID = "ARtELM9gW5sQcYEDb-11-jdoPAig6Y--f9Ggu9-RStP0QO4QrECMBHzzQaownQd1lZCL4yoJEOmYo8CN";
-// sb-j6jdb7896999@personal.example.com
-// e8O2lR-I
-const MP_CLIENT_ID = "";
-const MP_CLIENT_SECRET = "";
+const CLIENT_ID = "";
 ?>
